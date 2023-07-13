f2c422bd5744904ac4664f4f6e2c8dd911b11114
shadow2ali <mohamedaly.ma422@gmail.com>
2023-07-13 04:09:45 -0700

    intro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
+/**
+ * main -Entry point
+ *
+ *Description : A C programm that print put function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle%d\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
