92d8d17d7eedd9691dfb9430bda2b14b878b2e84
shadow2ali <mohamedaly.ma422@gmail.com>
2023-07-19 07:02:59 -0700

    i_will

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*print_alphabet - print alphabets in lowercase followed by a new line
+*Return: void
+*
+*/
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	printf("\n");
+	return (0);
+
+
+}
+
