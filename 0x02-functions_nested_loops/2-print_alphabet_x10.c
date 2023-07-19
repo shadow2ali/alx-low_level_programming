fe1fdd3091cb21cda70fad1234d0e4e07ccdb53d
shadow2ali <mohamedaly.ma422@gmail.com>
2023-07-19 07:29:06 -0700

    i_will

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
 */
 void print_alphabet_x10(void)
 {
-	int a;
-	char b;
+	int a, b;
+
 	for (a = 1; a <= 10; a++)
 	{
 		for (b = 'a'; b <= 'z'; b++)
 		{
 			_putchar(b);
 		}
-		printf("\n");
 	}
+	printf("\n");
 }
 
