b7607965ab4fc4bc3c9c346b0ccb82793eac0498
shadow2ali <mohamedaly.ma422@gmail.com>
2023-07-19 07:39:43 -0700

    i_will

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_islower -function takes a char and return output if lower or not.
+*@c: the char that we want to test
+*Return: 1 or 0 dependes lower or not
+*/
+int _islower(int c)
+{
+if (c > 97 && c < 123)
+return (1);
+else
+return (0);
+}
