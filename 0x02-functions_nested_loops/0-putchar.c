40e5e566e903adc052216acf94547585d4ca40db
shadow2ali <mohamedaly.ma422@gmail.com>
2023-07-19 03:43:09 -0700

    i_will

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main -Entry point
+ *
+ *Description : A C programm that print put function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
